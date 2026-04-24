462f2d3c036bc69c2615a74ee175a2c5c762e5c3
Ubuntu <ubuntu@localhost.localdomain>
2026-04-25 00:02:24 +0800

    add pdf parse declaration

---
+++ types/pdf-parse.d.ts
@@ -0,0 +1,12 @@
+declare module 'pdf-parse' {
+  function pdfParse(dataBuffer: Buffer): Promise<{
+    text: string
+    numpages: number
+    numrender: number
+    info: any
+    metadata: any
+    version: string
+  }>
+
+  export default pdfParse
+}
